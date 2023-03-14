41923ece62b531870e316da6c5c6199550d0cd6f
Jarre Owens <116134125+LuxeCapacitor@users.noreply.github.com>
2023-03-14 01:25:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-# This is the template code for the CNE335 Final Project
-# Justin Ellis
+# Final Automation Project
+# Jarre' Owens
 # CNE 335 Fall
+
+from Server import Server
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Jarre' Owens")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    server_ip = '3.145.34.41'
+
     # TODO - Call Ping method and print the results
+    my_server = Server(server_ip)
+    print(my_server.ping())

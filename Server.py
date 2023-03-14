bbe33958ea6f2d727516124e108cf53915122432
Jarre Owens <116134125+LuxeCapacitor@users.noreply.github.com>
2023-03-14 01:25:12 -0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,10 +1,21 @@
-class Server:
-    """ Server class for representing and manipulating servers. """
+# Final Automation Project
+# Jarre' Owens
+# CNE 335 Fall
 
+import os
+
+class Server:
+#   """ Server class for representing and manipulating servers. """
     def __init__(self, server_ip):
-        # TODO -
+# TODO -
         self.server_ip = server_ip
 
+# pings the server to verify connectivity
     def ping(self):
-        # TODO - Use os module to ping the server
-        return
+# TODO - Use os module to ping the server
+#https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
+#https://docs.python.org/3/library/os.html
+        ping_command = f"ping -n 5 {self.server_ip}"
+        ping_data = os.popen(ping_command).read()
+        print(ping_data)
+
